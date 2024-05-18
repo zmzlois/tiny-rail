104237766b620dc5055a4f15eeac1673e90c4672
zmzlois <lois.z@comcord.vision>
2024-05-18 20:42:58 +0100

    build succeed

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { HeroSection } from "../../components/hero";
 import { Header } from "@/components/header";
-import { auth } from "@/auth";
+
 import { redirect } from "next/navigation";
 
 export default async function Home() {
